c0fbbe74dbcabd39e8cea75368d2dbd6eb281e79
tefosemate <teferrasam@gmail.com>
2023-02-23 18:15:43 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,10 +3,8 @@
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9
  *
- *
  * Return: 0-9, excluding 2 and 4, followed by new line
  */
-
 void print_most_numbers(void)
 {
 	int n;
@@ -19,10 +17,10 @@ void print_most_numbers(void)
 		if ((n == 2) || (n == 4))
 		if ((i == 2) || (i == 4))
 			continue;
-		_putchar(i + '0');
-		_putchar('\n');
+			_putchar(i + '0');
+			_putchar('\n');
 		else
 			_putchar(i + '0');
 	}
-	_putchar('\n');
+			_putchar('\n');
 }
