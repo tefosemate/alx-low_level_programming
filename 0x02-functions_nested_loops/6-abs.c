05583145eafad0d97379216f367d794a9877a145
tefosemate <101450844+tefosemate@users.noreply.github.com>
2023-03-02 11:32:00 -0800

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - calculate the absolute value of the number.
+ * @i: the integer to be converted.
+ * Return: absolute value of the input number.
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		i = i * -1;
+	return (i);
+}
